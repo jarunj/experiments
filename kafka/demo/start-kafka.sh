96cfed948880994fbd63cbbb29321cca1e368c58
banshooter <banshooter@users.noreply.github.com>
2022-01-20 09:01:22 +0700

    Sink to file (#3)
    * add sink-file-local connector
    
    * add execute snapshot to mysql-source-connector

--- kafka/demo/start-kafka.sh
+++ kafka/demo/start-kafka.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # connector start command here.
-exec "/opt/kafka/bin/connect-standalone.sh" "/opt/kafka/config/connect.properties" "/opt/kafka/config/connect-source-mysql.properties" 
+exec "/opt/kafka/bin/connect-standalone.sh" "/opt/kafka/config/connect.properties" "/opt/kafka/config/connect-source-mysql.properties"  "/opt/kafka/config/connect-sink-file.properties" 
